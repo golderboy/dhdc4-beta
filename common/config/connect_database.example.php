147c4955e5d2b26173ca82a55d8753b0a088b4cb
golderboy <golderboy@gmail.com>
2026-07-15 21:41:44 +0700

    Add update server config template

---
+++ common/config/connect_database.example.php
@@ -0,0 +1,10 @@
+<?php
+// ตั้งค่าการเชื่อมต่อกับฐานข้อมูล
+
+$db_host = '127.0.0.1';
+$db_name = 'dhdc4';
+$db_port = 33061;
+$db_user = 'root';
+$db_pass = 'REDACTED_SECRET_8D969EEF6ECA';
+
+// จบการตั้งค่า
---
+++ common/config/connect_update.example.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=203.157.115.123;dbname=dhdc3;port=3309',
+    'username' => 'client',
+    'password' => '1234',
+    'charset' => 'utf8',
+];
+
